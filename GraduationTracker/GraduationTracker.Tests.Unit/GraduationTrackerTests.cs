0ae0f61f63b2f912a56c2f5e05fe0b7ebea75810
akhaikin <42449599+akhaikin@users.noreply.github.com>
2019-05-01 18:22:50 -0400

    Add files via upload

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -9,77 +9,74 @@ namespace GraduationTracker.Tests.Unit
     public class GraduationTrackerTests
     {
         [TestMethod]
+        public void TestRepositoryGetRequirements()
+        {
+            var requirements = Repository.GetRequirements();
+            Assert.AreEqual(4, requirements.Length);
+            Assert.AreEqual(100, requirements[0].Id);
+            Assert.AreEqual(102, requirements[1].Id);
+            Assert.AreEqual(103, requirements[2].Id);
+            Assert.AreEqual(104, requirements[3].Id);
+
+            Assert.AreEqual("Math", requirements[0].Name);
+            Assert.AreEqual("Science", requirements[1].Name);
+            Assert.AreEqual("Literature", requirements[2].Name);
+            Assert.AreEqual("Physichal Education", requirements[3].Name);
+        }
+
+        [TestMethod]
+        public void TestRepositoryGetStudent()
+        {
+            var student = Repository.GetStudent(-1);
+            Assert.AreEqual(null, student);
+            student = Repository.GetStudent(5);
+            Assert.AreEqual(null, student);
+
+            student = Repository.GetStudent(1);
+            Assert.AreEqual(1, student.Id);
+            Assert.AreEqual(4, student.Courses.Count);
+            for (int i1 = 0; i1 < 4; i1++)
+            {
+                int mark = student.Courses[i1].Mark;
+                Assert.AreEqual(95, mark);
+            }
+
+            student = Repository.GetStudent(2);
+            Assert.AreEqual(2, student.Id);
+            Assert.AreEqual(4, student.Courses.Count);
+            for (int i1 = 0; i1 < 4; i1++)
+            {
+                int mark = student.Courses[i1].Mark;
+                Assert.AreEqual(80, mark);
+            }
+        }
+
+        [TestMethod]
         public void TestHasCredits()
         {
-            var tracker = new GraduationTracker();
-
-            var diploma = new Diploma
+            var diploma = Repository.GetDiploma(1);
+            Assert.AreNotEqual(null, diploma);
+            var students = Repository.GetStudentsAll();
+            Assert.AreEqual(4, students.Length);
+            foreach (var student in students)
             {
-                Id = 1,
-                Credits = 4,
-                Requirements = new int[] { 100, 102, 103, 104 }
-            };
-
-            var students = new[]
-            {
-               new Student
-               {
-                   Id = 1,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=95 },
-                        new Course{Id = 2, Name = "Science", Mark=95 },
-                        new Course{Id = 3, Name = "Literature", Mark=95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
-                   }
-               },
-               new Student
-               {
-                   Id = 2,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=80 },
-                        new Course{Id = 2, Name = "Science", Mark=80 },
-                        new Course{Id = 3, Name = "Literature", Mark=80 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
-                   }
-               },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
-                }
+                Assert.AreEqual(4, student.Courses.Count);
             }
+            var tp = students[0].HasGraduated(diploma);
+            Assert.AreEqual(true, tp.Item1);
+            Assert.AreEqual(STANDING.SumaCumLaude, tp.Item2);
 
+            tp = students[1].HasGraduated(diploma);
+            Assert.AreEqual(true, tp.Item1);
+            Assert.AreEqual(STANDING.MagnaCumLaude, tp.Item2);
 
-            //tracker.HasGraduated()
-        };
-            
-            var graduated = new List<Tuple<bool, STANDING>>();
+            tp = students[2].HasGraduated(diploma);
+            Assert.AreEqual(true, tp.Item1);
+            Assert.AreEqual(STANDING.Average, tp.Item2);
 
-            foreach(var student in students)
-            {
-                graduated.Add(tracker.HasGraduated(diploma, student));      
-            }
-
-            
-            Assert.IsFalse(graduated.Any());
+            tp = students[3].HasGraduated(diploma);
+            Assert.AreEqual(false, tp.Item1);
+            Assert.AreEqual(STANDING.Remedial, tp.Item2);
 
         }
 
